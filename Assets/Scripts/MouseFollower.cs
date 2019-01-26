c2016134bc9402c7bca68eb713e967e79d6f4d95
Spoffy <spoffeh@gmail.com>
2019-01-26 15:37:22 +0000

    Adds Crosshair and MouseFollower script

---
+++ Assets/Scripts/MouseFollower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(worldPoint.x, worldPoint.y, 0);
+    }
+}
